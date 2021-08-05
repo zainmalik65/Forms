746f110add6bf7d472afcb0d211d0352aecd06cc
zain malik <zainmalik9964@gmail.com>
2021-08-05 12:47:59 -0700

    209 forms implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +17,16 @@ export class AppComponent implements OnInit {
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
       
     });
   }
   onSubmit()
   {
     console.log(this.signupForm);
+  }
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 }
