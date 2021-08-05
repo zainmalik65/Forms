54241632ef6f7edb7fd6378a991b0e90683331e6
zain malik <zainmalik9964@gmail.com>
2021-08-05 13:00:12 -0700

    210 forms implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm:FormGroup;
+  forbiddenUsernames=['Chris','Anna'];
   ngOnInit()
   {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required,this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -28,5 +29,13 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+  forbiddenNames(control:FormControl):{[s:string]:boolean}
+  {
+    if(this.forbiddenUsernames.indexOf(control.value)!==-1)
+    {
+      return {'nameIsForbiden':true}
+    }
+    return null;
   }
 }
